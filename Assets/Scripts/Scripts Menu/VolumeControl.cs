e3fa91549599c3fe7f2ed4a300766f7e0448bb45
Ragnafenrir <wargreymonx619@gmail.com>
2025-04-04 10:14:27 -0500

    opciones y audioplayer
    Slider de sfx funciona como debe, audioplayer añadido como prefab

--- Assets/Scripts/Scripts Menu/VolumeControl.cs
+++ Assets/Scripts/Scripts Menu/VolumeControl.cs
@@ -1,10 +1,17 @@
 using UnityEngine; //Importante
 using UnityEngine.UI; //Habilita implementar el Slider
-public class VolumeControl : MonoBehaviour
+using UnityEngine.EventSystems; //Habilita la funcion OnPointerUp
+
+public class VolumeControl : MonoBehaviour, IPointerUpHandler
 {
 
     Slider control;
+    public AudioClip sfxTest;
 
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        AudioManager.Instance.PlaySFX(sfxTest);
+    }
     void Start()
     {
         control = GetComponent<Slider>();
