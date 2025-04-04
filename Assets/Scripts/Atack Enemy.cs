10a7b61fc6fba7799182e748043065e6eb76b4c1
Retzmor <erwinsalas1278@gmail.com>
2025-04-04 11:57:02 -0500

    AnimacionEnemigoCOmpleto

--- Assets/Scripts/Atack Enemy.cs
+++ Assets/Scripts/Atack Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class AtackEnemy : MonoBehaviour
 {
-    Animation mushroom;
+    Animator mushroom;
     public int damage = 10;
     [SerializeField] Transform enemyPosition;
     [SerializeField] float radius;
@@ -15,7 +15,7 @@ public class AtackEnemy : MonoBehaviour
 
     void Start()
     {
-        mushroom = GetComponent<Animation>();
+        mushroom = GetComponent<Animator>();
         
     }
 
@@ -32,10 +32,11 @@ public class AtackEnemy : MonoBehaviour
 
 
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            mushroom.SetTrigger("attack");
             collision.gameObject.GetComponent<PlayerMovement>().RecibirDaño(damage);
         }
     }
