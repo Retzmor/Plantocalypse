b3615b6ed406801458df64b6f32df9a6dc8047ea
Retzmor <erwinsalas1278@gmail.com>
2025-04-04 11:23:38 -0500

    Falta la animacion de atacar
    Diego, aputaree : ) falta la animacion de ataar del enemigo

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    Animator mushRoom;
+    [SerializeField] bool takeDamage;
+    [SerializeField] bool movement;
+    [SerializeField] bool live = true;
     public float currentHealth;
+
     void Start()
     {
+        mushRoom = GetComponent<Animator>();
         currentHealth = health;
+
     }
 
     // Update is called once per frame
@@ -20,15 +28,29 @@ public class Enemy : MonoBehaviour
 
     public void DamageTake(int newDamage)
     {
-        Debug.Log("Recibiendo daño");
+      
         currentHealth -= newDamage;
+        takeDamage = true;
+        mushRoom.SetTrigger("Damage");
+           
         if (currentHealth <= 0)
         {
+            
             Death();
+            
         }
     }
 
+    
+
     public void Death()
+    {
+
+        live = false;
+        mushRoom.SetBool("Vida", live);
+      
+    }
+    public void DestroyCharacter()
     {
         Destroy(gameObject);
     }
