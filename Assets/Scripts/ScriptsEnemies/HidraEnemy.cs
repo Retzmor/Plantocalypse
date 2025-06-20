1db5beccc6df3cffc9358a355d139da9d10949f2
David15JD <erwinsalas1278@gmail.com>
2025-06-20 10:57:44 -0500

    Maquina de estados completa

--- Assets/Scripts/ScriptsEnemies/HidraEnemy.cs
+++ Assets/Scripts/ScriptsEnemies/HidraEnemy.cs
@@ -12,9 +12,12 @@ public class HidraEnemy : MonoBehaviour
     [SerializeField] private float _speed;
     Rigidbody2D _rb;
     Animator _animator;
-    private float health;
+    private float health = 100;
     private int damage = 10;
-    private float currentHealth;
+    private float currentHealth = 100;
+    [SerializeField] private float attackCooldown = 0.5f;
+    private float enfriamientoAtaque = -Mathf.Infinity;     
+
 
     public Rigidbody2D RbHidra { get => _rb; set => _rb = value; }
     public Animator AnimatorHidra { get => _animator; set => _animator = value; }
@@ -43,19 +46,30 @@ public class HidraEnemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
-        _animator.SetTrigger("Recibirdaño");          
+        _animator.SetTrigger("RecibirDaño");
+        Debug.Log(CurrentHealth);
         if (CurrentHealth <= 0)
-        _animator.SetTrigger("Muerte");       
+        {
+            _animator.SetBool("Muerte", true);
+            _animator.SetBool("Atacar", false);
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
+            PlayerMovement jugador = collision.GetComponent<PlayerMovement>();
+
+            if (jugador != null && Time.time - enfriamientoAtaque >= attackCooldown)
+            {
+                jugador.RecibirDaño(damage);
+                enfriamientoAtaque = Time.time;
+            }
+
             _animator.SetBool("Atacar", true);
         }
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -70,3 +84,5 @@ public class HidraEnemy : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
+
+   
