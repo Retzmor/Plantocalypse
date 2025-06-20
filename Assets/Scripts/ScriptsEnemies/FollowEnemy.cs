ac534f6a9789dc0c4c78da92f71acffd8daf754c
Retzmor <erwinsalas1278@gmail.com>
2025-06-19 21:22:59 -0500

    MovimientoEnemigo

--- Assets/Scripts/ScriptsEnemies/FollowEnemy.cs
+++ Assets/Scripts/ScriptsEnemies/FollowEnemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class FollowEnemy : StateMachineBehaviour
 {
@@ -15,12 +16,23 @@ public class FollowEnemy : StateMachineBehaviour
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Debug.Log("Estoy en el update");
+    { 
         Vector2 direccion = (HidraEnemy.TargetHidra.transform.position - rb.transform.position).normalized;
-        Vector2 nuevaDireccion = rb.position + direccion * HidraEnemy.SpeedHidra * Time.deltaTime;
+        Vector2 nuevaDireccion = rb.position + direccion * HidraEnemy.SpeedHidra * Time.fixedDeltaTime;
         rb.MovePosition(nuevaDireccion);
-        Debug.Log(nuevaDireccion);
+
+        Vector3 rotacionActual = rb.transform.localScale;
+        float diferenciaX = HidraEnemy.TargetHidra.transform.position.x - rb.transform.position.x;
+
+        if (diferenciaX < 0)
+        {
+            rb.transform.localScale = new Vector3(Mathf.Abs(rotacionActual.x), rotacionActual.y, rotacionActual.z);
+        }
+        else if (diferenciaX > 0)
+        {
+            rb.transform.localScale = new Vector3(-Mathf.Abs(rotacionActual.x), rotacionActual.y, rotacionActual.z);
+        }
+
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
